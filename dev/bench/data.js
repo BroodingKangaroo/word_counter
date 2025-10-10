0f959f16edf597c82c69e9fd371ec2d800939130
github-action-benchmark <github@users.noreply.github.com>
2025-10-10 12:12:48 +0000

    add Word Counter Benchmark (Time) (customSmallerIsBetter) benchmark result for c6328ffeea46973d1ced01a0d19bc98665ed5876

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1760098368710,
+  "repoUrl": "https://github.com/BroodingKangaroo/word_counter",
+  "entries": {
+    "Word Counter Benchmark (Time)": [
+      {
+        "commit": {
+          "author": {
+            "email": "johngolt33@gmail.com",
+            "name": "BroodingKangaroo",
+            "username": "BroodingKangaroo"
+          },
+          "committer": {
+            "email": "johngolt33@gmail.com",
+            "name": "BroodingKangaroo",
+            "username": "BroodingKangaroo"
+          },
+          "distinct": true,
+          "id": "c6328ffeea46973d1ced01a0d19bc98665ed5876",
+          "message": "Remove benchmark results",
+          "timestamp": "2025-10-10T14:38:54+03:00",
+          "tree_id": "c9563f79bcb3536d0fbf5a4b6e4f4c7af7e3e6e5",
+          "url": "https://github.com/BroodingKangaroo/word_counter/commit/c6328ffeea46973d1ced01a0d19bc98665ed5876"
+        },
+        "date": 1760098367709,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "'../build/benchmarks/word_counter_bench' 'enwiki-multistream.xml' 'output.txt'",
+            "value": 158.80484615187999,
+            "unit": "s"
+          }
+        ]
+      }
+    ]
+  }
+}
