bbb9fea3b1e250cb07e0c2e41870a513b4b72ae4
github-action-benchmark <github@users.noreply.github.com>
2025-10-11 23:08:36 +0000

    add Word Counter Benchmark (Time) (customSmallerIsBetter) benchmark result for d1493a0db36c549cfcbdfa18084396db7a42e017

---
+++ data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1760224116114,
+  "repoUrl": "https://github.com/BroodingKangaroo/word_counter",
+  "entries": {
+    "Word Counter Benchmark (Time)": [
+      {
+        "commit": {
+          "author": {
+            "email": "johngolt33@gmail.com",
+            "name": "BroodingKangaroo",
+            "username": "BroodingKangaroo"
+          },
+          "committer": {
+            "email": "johngolt33@gmail.com",
+            "name": "BroodingKangaroo",
+            "username": "BroodingKangaroo"
+          },
+          "distinct": true,
+          "id": "d1493a0db36c549cfcbdfa18084396db7a42e017",
+          "message": "Naive: trigger benchmark",
+          "timestamp": "2025-10-12T01:08:21+03:00",
+          "tree_id": "a8d81846dba955ff64c60fed7b8fb454b29379d6",
+          "url": "https://github.com/BroodingKangaroo/word_counter/commit/d1493a0db36c549cfcbdfa18084396db7a42e017"
+        },
+        "date": 1760224115752,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Naive Word Counter Implementation (Time)",
+            "value": 239.66,
+            "unit": "s"
+          }
+        ]
+      }
+    ]
+  }
+}
