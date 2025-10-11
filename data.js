819359f43e9f938d8a5d0303a4571d66c4689ea3
github-action-benchmark <github@users.noreply.github.com>
2025-10-11 19:57:58 +0000

    add Word Counter Benchmark (Time) (customSmallerIsBetter) benchmark result for 136fd7ebbd3681beddceb08c00609dab3e8b9e3a

---
+++ data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1760212678174,
+  "repoUrl": "https://github.com/BroodingKangaroo/word_counter",
+  "entries": {
+    "Word Counter Benchmark (Time)": [
+      {
+        "commit": {
+          "author": {
+            "email": "35418986+BroodingKangaroo@users.noreply.github.com",
+            "name": "Maksim Halavaty",
+            "username": "BroodingKangaroo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "136fd7ebbd3681beddceb08c00609dab3e8b9e3a",
+          "message": "Naive: Change graph path and change name. Round time to 2nd floating-point digit. Add description for naive solution.\n\n* Naive: Update naming and README.md\n\n* Rename files to have numbers in implementations. Add link to gh-pages to README.md\n\n* Trigger --release\n\n* Naive: Update include in tests.cpp\n\n* Naive: Change graph path and change name. Round time to 2nd floating-point digit. Add description for naive solution.",
+          "timestamp": "2025-10-11T22:53:53+03:00",
+          "tree_id": "cffc725d8fc13a10dfc252563258e4ab425ba085",
+          "url": "https://github.com/BroodingKangaroo/word_counter/commit/136fd7ebbd3681beddceb08c00609dab3e8b9e3a"
+        },
+        "date": 1760212677738,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Naive Word Counter Implementation (Time)",
+            "value": 5.29,
+            "unit": "s"
+          }
+        ]
+      }
+    ]
+  }
+}
