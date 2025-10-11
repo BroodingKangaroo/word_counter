c8e11133b0f57d7abf5883209d797750c3016def
github-action-benchmark <github@users.noreply.github.com>
2025-10-11 19:23:27 +0000

    add Word Counter Benchmark (Time) (customSmallerIsBetter) benchmark result for ffe5b92d8b19addd68d9803ad646a881ce38d3d1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1760098368710,
+  "lastUpdate": 1760210607218,
   "repoUrl": "https://github.com/BroodingKangaroo/word_counter",
   "entries": {
     "Word Counter Benchmark (Time)": [
@@ -28,6 +28,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "'../build/benchmarks/word_counter_bench' 'enwiki-multistream.xml' 'output.txt'",
             "value": 158.80484615187999,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "35418986+BroodingKangaroo@users.noreply.github.com",
+            "name": "Maksim Halavaty",
+            "username": "BroodingKangaroo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ffe5b92d8b19addd68d9803ad646a881ce38d3d1",
+          "message": "Naive: Update include in tests.cpp",
+          "timestamp": "2025-10-11T21:42:27+03:00",
+          "tree_id": "642752783c0af88de9dc4589446c7477204680c7",
+          "url": "https://github.com/BroodingKangaroo/word_counter/commit/ffe5b92d8b19addd68d9803ad646a881ce38d3d1"
+        },
+        "date": 1760210606875,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "'../build/benchmarks/word_counter_bench' 'enwiki-multistream.xml' 'output.txt'",
+            "value": 139.93217427312,
             "unit": "s"
           }
         ]
